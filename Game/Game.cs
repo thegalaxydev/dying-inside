6d19355c8223600a49e289b8ccd64c0252ea7c90
Galaxy <bryon.guidry1@gmail.com>
2022-11-04 12:55:29 -0500

    Established sprites

---
+++ Game/Game.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using GameEngine;
+using MathLibrary;
+
+namespace DyingInside
+{
+    internal class Game
+    {
+        void Start()
+        {
+
+        }
+        public void Run()
+        {
+            Start();
+        }
+    }
+}
---
+++ Game/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using GameEngine;
+using MathLibrary;
+
+namespace DyingInside
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Game game = new Game();
+
+            game.Run();
+        }
+    }
+}
